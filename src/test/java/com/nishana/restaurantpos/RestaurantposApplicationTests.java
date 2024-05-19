ac3e8560901a575e25b9a3dfbdaba8fc95335f83
jitendra977 <jitendrakhadka@Jitendras-Mac-mini.local>
2024-05-20 08:09:19 +0900

    first

---
+++ src/test/java/com/nishana/restaurantpos/RestaurantposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishana.restaurantpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
