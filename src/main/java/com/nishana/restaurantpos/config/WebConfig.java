1b4a423be4963a7ac967a4af1601a161bb26dadc
jitendra977 <jitendrakhadka4@gmail.com>
2024-07-15 00:34:06 +0000

    work all condition jul 15

--- src/main/java/com/nishana/restaurantpos/config/WebConfig.java
+++ src/main/java/com/nishana/restaurantpos/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://82.180.161.107")
+                .allowedOrigins("http://82.180.161.107","http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
