7163388536e3e5dc04f23b11fe943dfcb2060bee
Jit bahadur khadka <jitendrakhadka4@gmail.com>
2024-08-10 17:08:00 +0900

    menuitemdto

--- src/main/java/com/nishana/restaurantpos/dto/MenuItemDTO.java
+++ src/main/java/com/nishana/restaurantpos/dto/MenuItemDTO.java
@@ -4,10 +4,12 @@ import lombok.Data;
 
 @Data
 public class MenuItemDTO {
+    private int itemId;
     private String name;
     private String description;
     private double price;
     private Long categoryId;
+    private String categoryName;
     private String availability;
     
 }
